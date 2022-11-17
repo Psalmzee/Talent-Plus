7e8ddc3d9ba8fd6bb0b4c9e3529cda062cff132e
Samson Okeji <engrsamsonokeji@gmail.com>
2022-11-17 08:27:49 +0100

    major update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+   const [posts, setPosts] = useState([]);
+   useEffect(() => {
+      fetch('http://localhost:9002/api/texts')
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setPosts(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+
+return (
+
+   <div className="posts-container">
+    <h1 className='app-title'>Talent-Plus</h1>
+    <p ClassName = 'sub-title'> This is a Frontend Microservice that fetches data from an API call</p>
+      {posts.map((post) => {
+         return (
+            <div className="post-card" key={post.id}>
+               <h2 className="post-title">{post.title}</h2>
+               <p className="post-body">{post.body}</p>
+               <div className="button">
+               <div className="delete-btn">Delete</div>
+               </div>
+            </div>
+         );
+      })}
+   </div>
+   );
+};
 
 export default App;
