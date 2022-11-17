f39ce1be8620e1091bc691840eed93eeb29e2fc9
Samson Okeji <engrsamsonokeji@gmail.com>
2022-11-17 09:06:30 +0100

    little changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
 
 const App = () => {
-   const [posts, setPosts] = useState([]);
-   useEffect(() => {
-      fetch('http://localhost:9002/api/texts')
-         .then((response) => response.json())
-         .then((data) => {
-            console.log(data);
-            setPosts(data);
-         })
-         .catch((err) => {
-            console.log(err.message);
-         });
-   }, []);
+     const baseURL = "http://localhost:9002/api";
+   
+     const [getResult, setGetResult] = useState(null);
+   
+     const fortmatResponse = (res) => {
+       return JSON.stringify(res, null, 2);
+     }
+   
+     async function getAllData() {
+       try {
+         const res = await fetch(`${baseURL}/texts`);
+   
+         if (!res.ok) {
+           const message = `An error has occured: ${res.status} - ${res.statusText}`;
+           throw new Error(message);
+         }
+   
+         const data = await res.json();
+   
+         const result = {
+           status: res.status + "-" + res.statusText,
+           headers: {
+             "Content-Type": res.headers.get("Content-Type"),
+             "Content-Length": res.headers.get("Content-Length"),
+           },
+           length: res.headers.get("Content-Length"),
+           data: data,
+         };
+   
+         setGetResult(fortmatResponse(result));
+       } catch (err) {
+         setGetResult(err.message);
+       }
+     }
 
 return (
 
-   <div className="posts-container">
-    <h1 className='app-title'>Talent-Plus</h1>
-    <p ClassName = 'sub-title'> This is a Frontend Microservice that fetches data from an API call</p>
-      {posts.map((post) => {
-         return (
-            <div className="post-card" key={post.id}>
-               <h2 className="post-title">{post.title}</h2>
-               <p className="post-body">{post.body}</p>
-               <div className="button">
-               <div className="delete-btn">Delete</div>
-               </div>
-            </div>
-         );
-      })}
-   </div>
+   <div className="card">
+      <div className="card-header">TALENT PLUS MICROSERVICES</div>
+      <div className="card-body">
+        <div className="input-group input-group-sm">
+          <button className="btn btn-sm btn-primary" onClick={getAllData}>Get TALENT PLUS</button>
+
+        { getResult && <div className="alert alert-secondary mt-2" role="alert"><pre>{getResult}</pre></div> }
+      </div>
+    </div>
+    </div> 
+
    );
 };
 
